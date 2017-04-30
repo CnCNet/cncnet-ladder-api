1580decd39b8d23b293f31fcc57f04553abc0d51
Grant Bartlett <hello@grant-bartlett.com>
2017-04-30 18:08:57 +0100

    first look at stats parsing

--- cncnet-api/app/Http/Controllers/ApiLadderController.php
+++ cncnet-api/app/Http/Controllers/ApiLadderController.php
@@ -23,9 +23,97 @@ class ApiLadderController extends Controller
         return $this->ladderService->getLadderByGameAbbreviation($game);
     }
 
+    // Credit: https://github.com/dkeetonx
     public function postLadder(Request $request)
     {
-        // todo
+        // TODO - handle request and saving
+
+        $file = "stats_ts.dmp";
+        $fh = fopen($file, "r");
+
+        $data = fread($fh, 4);
+
+        if (!$data) {
+           return;
+        }
+
+        $offset = 4;
+        $stats_ver = unpack("V", $data)[1];
+
+        print "Stats version = $stats_ver\n";
+
+        $pad = 0;
+
+        while (!feof($fh)) 
+        {
+            $data = fread($fh, 8);
+            if (!$data) 
+            {
+                // exit loop here
+                print "\$data filed";
+                break;
+            }
+
+            $ttl = unpack("A4tag/ntype/nlength", $data);
+            $pad = ($ttl["length"] % 4) ? 4 - ($ttl["length"] %  4) : 0;
+
+            print "$ttl[tag] $ttl[type] $ttl[length]";
+
+            if ($ttl["length"] > 0) 
+            {
+                $data = fread($fh, $ttl["length"]);
+
+                if ($pad > 0 ) 
+                {
+                    fread($fh, $pad);
+                }
+
+                switch ($ttl["type"]) 
+                {
+                    case 20:
+                        $v = unpack("C$ttl[length]", $data);
+                        print "\t";
+                        print "<<raw data HERE>>";
+                        break;
+                    case 1:
+                        $v = unpack("C", $data);
+                        print "\t$v[1]";
+                        break;
+                    case 2:
+                        $v = unpack("C", $data);
+                        if ($v[1] == 0) {
+                            print "\tFalse";
+                        }
+                        else {
+                            print "\tTrue";
+                        }
+                        break;
+                    case 3:
+                        $v = unpack("n", $data);
+                        print "\t$v[1]";
+                        break;
+                    case 4:
+                        $v = unpack("n", $data);
+                        print "\t$v[1]";
+                        break;
+                    case 5:
+                        $v = unpack("N", $data);
+                        print "\t$v[1]";
+                        break;
+                    case 6:
+                        $v = unpack("N", $data);
+                        print "\t$v[1]";
+                        break;
+                    case 7:
+                        $ttl["length"] -= 1;
+                        $v = unpack("a$ttl[length]", $data);
+                        print "\t$v[1]";
+                        break;
+                }
+            }
+
+            print "\n";
+        }
     }
 
     public function getLadderGame(Request $request, $game = null, $gameId = null)
