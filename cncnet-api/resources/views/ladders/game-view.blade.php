101f35d6f31d3784484b3f3f1040fc6e05ea9f28
Grant Bartlett <hello@grant-bartlett.com>
2017-07-23 10:32:43 +0100

    added funds left to view

--- cncnet-api/resources/views/ladders/game-view.blade.php
+++ cncnet-api/resources/views/ladders/game-view.blade.php
@@ -125,7 +125,7 @@
                         <p class="points"><i class="fa fa-bolt fa-fw"></i> {{ $points  }}</p>
                         <p class="points">
                             <?php $credits = json_decode($gameStats->crd); ?>
-                            <i class="fa fa-usd fa-fw" aria-hidden="true"></i> {{ $credits->value }}
+                            <strong>Funds Left: </strong> {{ $credits->value }} 
                         </p>
                         <p class="colour player-panel-{{ $gameStats->colour($gameStats->col) }}" style="width:25px;height:25px;"></p>
                         <div class="country">
