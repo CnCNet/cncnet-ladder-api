21f9109759477e6edfcbc58ae49ff1e2928676cb
Daniel Keeton Jr <dakeeton@gmail.com>
2017-09-21 15:01:12 -0500

    Adds common maps to matchup criteria.

--- cncnet-api/app/Http/Controllers/ApiQuickMatchController.php
+++ cncnet-api/app/Http/Controllers/ApiQuickMatchController.php
@@ -149,6 +149,7 @@ class ApiQuickMatchController extends Controller
                 $qmOpns = \App\QmMatchPlayer::where('qm_match_players.id', '<>', $qmPlayer->id)
                         ->where('waiting', true)
                         ->where('ladder_id', $qmPlayer->ladder_id)
+                        ->whereRaw("map_bitfield & {$qmPlayer->map_bitfield} > 0")
                         ->whereNull('qm_match_id')
                         ->join('player_ratings', 'player_ratings.player_id', '=', 'qm_match_players.player_id')
                         ->selectRAW( "qm_match_players.id as id, waiting, qm_match_players.player_id as player_id,"
