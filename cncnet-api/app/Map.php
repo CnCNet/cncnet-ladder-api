0a4a467fd69bc5ac124cf73372a8d73cbfdfb482
Daniel Keeton Jr <dakeeton@gmail.com>
2017-10-22 08:46:21 -0500

    Fixed a bug where per map sides didn't work for player.

--- cncnet-api/app/Map.php
+++ cncnet-api/app/Map.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Map extends Model 
+class Map extends Model
 {
     protected $table = 'maps';
     protected $fillable = ['name', 'hash'];
     public $timestamps = false;
+
+    public function qmMaps()
+    {
+        return $this->hasMany('App\QmMap');
+    }
 }
