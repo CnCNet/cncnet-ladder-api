e92cb2201d33704183933b6467de48401c8a7f47
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2017-10-23 16:19:38 +0200

    fix broken yr cameo

--- cncnet-api/config/cameos.php
+++ cncnet-api/config/cameos.php
@@ -415,7 +415,7 @@ return
         "FV" =>	"fvicon",
         "HTK" =>	"htkicon",
         "HTNK" =>	"htnkicon",
-        "MTNK" =>	"mtnkicon",
+        "MTNK" =>	"gtnkicon",
         "TTNK" =>	"ttnkicon",
         "TNKD" =>	"tnkdicon",
         "GTNK" =>	"gtnkicon",
