976496ed9d06958e76985b96215fc22e133e4866
Grant Bartlett <hello@grant-bartlett.com>
2017-10-29 13:52:27 +0000

    Bump css version

--- cncnet-api/resources/views/layouts/app.blade.php
+++ cncnet-api/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <title>@yield('title') - CnCNet</title>
     @yield('css')
     <link rel="stylesheet" href="/css/app.css" />
-    <link rel="stylesheet" href="/css/ladder.css?v=1.5" />
+    <link rel="stylesheet" href="/css/ladder.css?v=1.6" />
     <link rel="stylesheet" href="/css/font-awesome.min.css" />
     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/images/favicon/favicon-32x32.png" sizes="32x32">
