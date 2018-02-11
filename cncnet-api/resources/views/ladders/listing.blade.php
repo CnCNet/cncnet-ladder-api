7b8f82a80b894d8cd37df566c3844d791e1abb97
Grant Bartlett <hello@grant-bartlett.com>
2018-02-11 17:16:12 +0000

    Add winners to previous ladder pages
    Added a feature to showcase the winners of a previous month

--- cncnet-api/resources/views/ladders/listing.blade.php
+++ cncnet-api/resources/views/ladders/listing.blade.php
@@ -31,12 +31,47 @@
 @section('content')
 <section class="cncnet-features general-texture game-detail">
     <div class="container">
+        <?php $date = \Carbon\Carbon::parse($history->ends); ?>
+
+        @if ($date->isPast())
+        <h2><strong>{{ $date->format("m/Y") }}</strong> League Champions!</h2>
         <div class="feature">
             <div class="row">
-                <div class="col-md-12">
-                    <h3><strong>Follow all Ladder Changes</strong> <a href="https://forums.cncnet.org/forum/66-cncnet-ladder/">in our forum</a></h3>
+                <?php $winners = $players->slice(0,2); ?>
+                @foreach($winners as $k => $winner)
+                <div class="col-xs-12 col-md-6">
+                    <div class="ladder-cover cover-{{ $history->ladder->abbreviation}}" style="background-image: url('/images/ladder/{{ $history->ladder->abbreviation . "-cover-masters.png" }}')">
+                        <div class="details tier-league-cards">
+                            <div class="type">
+                                <h1 class="lead"><strong>{{ $winner->username }}</strong></h1>
+                                <h2><strong>Rank #{{ $k+1 }}</strong></h2>
+                                <ul class="list-inline" style="font-size: 14px;">
+                                    <li>
+                                        Wins 
+                                        <i class="fa fa-level-up"></i> {{ $winner->total_wins }}
+                                    </li>
+                                    <li>
+                                        Games 
+                                        <i class="fa fa-diamond"></i> {{ $winner->total_games }}
+                                    </li>
+                                </ul>
+                                @if ($k>0)
+                                <small>Runner up of the 
+                                @else
+                                <small>Champion of the 
+                                @endif
+                                <strong>{{ $date->format("m/Y") }}</strong> {{ $history->game }} League</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                @endforeach
+            </div>
+        </div>
+        @endif
 
+        <div class="feature">
+            <div class="row">
                 <div class="header">
                     <div class="col-md-12">
                         <h3><strong>1vs1</strong> Recent Games 
