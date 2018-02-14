97004200bdd6faa8b17aa37d367534afe877b582
Grant Bartlett <hello@grant-bartlett.com>
2018-02-14 22:22:31 +0000

    Update recent-games widget

--- cncnet-api/public/js/cncnet-recent-games.js
+++ cncnet-api/public/js/cncnet-recent-games.js
@@ -1,51 +1,56 @@
 // Fetch Recent Ladder Games Example
 (function ()
 {
-    var baseUrl = "http://ladder.cncnet.org/api/v1/ladder/yr/games/recent/5";
+    var baseUrl = "//staging.cnc-comm.com/api/v1/ladder/yr/games/recent/5";
 
     function onGetRecentGames($game)
     {
-        $.ajax
-        ({ 
-            url: baseUrl 
-        })
-        .complete((response) => onRecentGamesReceived(response))
-        .error((err) => onRecentGamesErrorReceived(err));
+        $.ajax({ url: baseUrl, dataType: "json", crossDomain: true })
+            .done((games) => onRecentGamesReceived(games))
+            .fail((error) => onRecentGamesErrorReceived(error));
     }
 
     function onRecentGamesReceived(response)
     {
         var games = response;
-        var el = document.getElementById("recent-games");
+        var gamesList = document.getElementById("recent-games-list");
 
         for (var i = 0; i < games.length; i++)
         {
             var game = games[i];
-            var li = document.createElement("li");
-            li.classList.add("game-box");
 
-            updateList(li, game.scen, "h4", "title");
+            // Create list item
+            var gameBoxContainer = document.createElement("li");
+            gameBoxContainer.classList.add("game-box");
+            
+            // Create players container
+            var playersContainer = document.createElement("div");
+            playersContainer.classList.add("players");
+            
+            updateGamesList(gameBoxContainer, game.scen, "h4", "title");
 
             for (var j = 0; j < game.players.length; j++)
             {
                 var player = game.players[j];
-                var pElement;
+                var pointsContainer;
 
                 if (player.won)
                 {
-                    var points = updateList(li, "+" + player.points, "span", "points");
-                    pElement = updateList(li, player.username, "h5", "player won");
+                    var points = updateGamesList(gameBoxContainer, "+" + player.points, "span", "points");
+                    pointsContainer = updateGamesList(gameBoxContainer, player.username, "h5", "player won");
                 }
                 else
                 {
-                    var points = updateList(li, player.points, "span", "points");
-                    pElement = updateList(li, player.username, "h5", "player lost");
+                    var points = updateGamesList(gameBoxContainer, player.points, "span", "points");
+                    pointsContainer = updateGamesList(gameBoxContainer, player.username, "h5", "player lost");
                 }
 
-                pElement.appendChild(points);
+                pointsContainer.appendChild(points);
+                playersContainer.appendChild(pointsContainer);
             }
 
-            el.appendChild(li);
+            gameBoxContainer.appendChild(playersContainer);
+            gamesList.appendChild(gameBoxContainer);
         }
     }
 
@@ -54,19 +59,19 @@
         console.log("Error - ", error);
     }
 
-    function updateList(element, text, type, className)
+    function updateGamesList(gameListElement, text, type, className)
     {
-        var el = document.createElement(type);
+        var gamesList = document.createElement(type);
         if (text != null)
         {
-            el.innerText = text;
+            gamesList.innerText = text;
             if (className != null)
             {
-                el.className = className;
+                gamesList.className = className;
             }
-            element.appendChild(el);
+            gameListElement.appendChild(gamesList);
         }
-        return el;
+        return gamesList;
     }
 
     onGetRecentGames();
