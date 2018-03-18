18245d3cf9dc4234760bbb44c872bbf170b329a2
Daniel Keeton Jr <dakeeton@gmail.com>
2018-03-18 18:19:29 -0500

    Adds user_name to the token response.

--- cncnet-api/app/Http/Controllers/ApiAuthController.php
+++ cncnet-api/app/Http/Controllers/ApiAuthController.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
-class ApiAuthController extends Controller 
+class ApiAuthController extends Controller
 {
     public function __construct()
     {
 
     }
-    
+
     public function getAuth(Request $request)
     {
         $user = \Auth::user();
+        $user_name = $user->name;
         $token = JWTAuth::fromUser($user);
-        return response()->json(compact('token'));
+        return response()->json(compact('token', 'user_name'));
     }
 
     public function putAuth(Request $request, $player = null)
