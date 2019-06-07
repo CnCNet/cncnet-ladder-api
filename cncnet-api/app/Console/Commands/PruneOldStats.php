25e1434a12242740dda46ceb1639a142806a25f2
Daniel Keeton Jr <dakeeton@gmail.com>
2019-06-06 22:40:20 -0500

    Minor bug fixes.

--- cncnet-api/app/Console/Commands/PruneOldStats.php
+++ cncnet-api/app/Console/Commands/PruneOldStats.php
@@ -11,7 +11,7 @@ class PruneOldStats extends Command {
 	 *
 	 * @var string
 	 */
-	protected $name = 'command:prune_stats';
+	protected $name = 'prune_stats';
 
 	/**
 	 * The console command description.
