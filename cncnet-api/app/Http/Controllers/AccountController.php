73522bd9da0d5cb4caf3ee2a676bb97996d7783d
Grant Bartlett <hello@grant-bartlett.com>
2019-08-07 00:54:46 +0100

    Add first pass around logic activating/deactivating usernames

--- cncnet-api/app/Http/Controllers/AccountController.php
+++ cncnet-api/app/Http/Controllers/AccountController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use \App\Http\Services\PlayerService;
 use \App\Http\Services\LadderService;
 use \App\EmailVerification;
+use Carbon\Carbon;
 use Mail;
 
 class AccountController extends Controller
@@ -42,14 +43,116 @@ class AccountController extends Controller
             'ladder' => 'required|string|',
             'username' => 'required|string|regex:/^[a-zA-Z0-9_\[\]\{\}\^\`\-\\x7c]+$/|max:11', //\x7c = | aka pipe
         ]);
-
+        
+        $ladderId = $request->ladder;
         $user = \Auth::user();
-        $username = $this->playerService->addPlayerToUser($request->username, $user, $request->ladder);
+        $player = $this->playerService->addPlayerToUser($request->username, $user, $ladderId);
 
-        if ($username == null)
+        if ($player == null)
         {
              $request->session()->flash('error', 'This username has been taken');
+            return redirect("/account");
         }
+
+        // If we're creating a username for the first time for this ladder type
+        $isNewUser = \App\Player::where("user_id", $user->id)
+            ->where("ladder_id", $ladderId)
+            ->count();
+
+        if ($isNewUser == 1)
+        {
+            // Make it active by default
+            $activeHandle = new \App\PlayerActiveHandle();
+            $activeHandle->ladder_id = $ladderId;
+            $activeHandle->player_id = $player->id;
+            $activeHandle->user_id = $user->id;
+            $activeHandle->save();
+        }
+        
+        if ($player == null)
+        {
+             $request->session()->flash('error', 'This username has been taken');
+            return redirect("/account");
+        }
+
+        $request->session()->flash('success', 'Username created!');
+        return redirect("/account");
+    }
+
+    public function toggleUsernameStatus(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required|string',
+            'ladderId' => 'required|string'
+        ]);
+        
+        $user = \Auth::user();
+        if ($user == null)
+        {
+            return redirect("/account");
+        }
+
+        $ladderId = $request->ladderId;
+        $username = $request->username;
+        
+        // Check request is linked to the auth'd user
+        $player = \App\Player::where("username", "=", $username)
+            ->where("ladder_id", "=", $ladderId)
+            ->where("user_id", "=", $user->id)
+            ->first();
+            
+        if ($player == null)
+        {
+            $request->session()->flash('error', 'Unknown error');
+            return;
+        }
+
+        $date = Carbon::now();
+        $endOfMonth = $date->endOfMonth()->toDateTimeString();
+
+        // Check if there are active handles within this month 
+        $hasActiveHandles = \App\PlayerActiveHandle::where("ladder_id", $ladderId)
+            ->where("user_id", $user->id)
+            ->where("created_at", "<=", $endOfMonth)
+            ->first();
+
+        if ($hasActiveHandles)
+        {
+            $request->session()->flash('error', 'You have a username active for this month and ladder already. 
+                If you are trying to make a username inactive, 
+                the month we are in has to complete first.');
+            return redirect("/account");
+        }
+        
+
+        // Get the player thats being requested to change
+        $activeHandle = \App\PlayerActiveHandle::where("player_id", $player->id)
+            ->where("ladder_id", $ladderId)
+            ->first();
+
+
+        // If it's an active handle make it one
+        if ($activeHandle == null)
+        {
+            $activeHandle = new \App\PlayerActiveHandle();
+            $activeHandle->ladder_id = $ladderId;
+            $activeHandle->player_id = $player->id;
+            $activeHandle->user_id = $user->id;
+            $activeHandle->save();
+
+            $request->session()->flash('success', $player->username . ' is now active on the ladder.');
+            return redirect("/account");
+        }
+
+        if ($activeHandle->created_at > $endOfMonth)
+        {
+            $activeHandle->delete();
+
+            $request->session()->flash('success', $player->username . ' is now inactive');
+            return redirect("/account");
+        }
+
+        $request->session()->flash('error', $player->username . ' is still active for this month');
         return redirect("/account");
     }
 
