d0eea49b6d88b341cc50c17dcde6a27f092f88da
Grant Bartlett <hello@grant-bartlett.com>
2020-05-31 11:13:16 +0100

    Bling bling countdown

--- cncnet-api/public/js/cncnet-countdown.js
+++ cncnet-api/public/js/cncnet-countdown.js
@@ -1,38 +1,74 @@
+(function ()
+{
 
-$(".close-countdown").click( function(){ $(".countdown").hide(400); });
+    let countDown = document.querySelector(".countdown");
+    let closeCountDownBtn = document.querySelector(".close-countdown");
+    closeCountDownBtn.addEventListener("click", function () { hideCountDown(); }, false);
 
-$(".countdown-fill").each(
-    function ()
+    function hideCountDown()
     {
-        var endDate = new Date($(this).attr("data-countdown-target"));
-        var elem = this;
+        countDown.classList.add("countdown-hidden")
+        window.localStorage.setItem("hideCountDown", true);
+    }
 
-        var timing = setInterval(
-            function () {
-                var currentDate = new Date().getTime();
-                var timeLeft = endDate - currentDate;
+    function shouldShowCountDown()
+    {
+        let key = window.localStorage.getItem("hideCountDown");
+        if (key == null)
+        {
+            return true;
+        }
+        return key == "false";
+    }
+
+    function showCountDown()
+    {
+        countDown.classList.remove("countdown-hidden");
+    }
+
+    let countDownFill = document.querySelectorAll(".countdown-fill");
+    for (let i = 0; i < countDownFill.length; i++)
+    {
+        let elem = countDownFill[i];
+        let date = elem.getAttribute("data-countdown-target");
+        let endDate = new Date(date);
+
+        let timing = setInterval(
+            function ()
+            {
+                let currentDate = new Date().getTime();
+                let timeLeft = endDate - currentDate;
+
+                if (shouldShowCountDown() == false)
+                {
+                    clearInterval(timing);
+                    return;
+                }
 
                 if (timeLeft < 86400 * 2 * 1000)
-                    $(".collapse.countdown").show();
+                {
+                    showCountDown();
+                }
 
-                var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-                if (seconds < 10) seconds="0"+seconds;
+                let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+                let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+                if (seconds < 10) seconds = "0" + seconds;
 
-                var str = "";
-                if (days > 0)     str += days + "d ";
-                if (hours > 0)    str += hours + "h ";
-                if (minutes > 0)  str += minutes + "m ";
+                let str = "";
+                if (days > 0) str += days + "d ";
+                if (hours > 0) str += hours + "h ";
+                if (minutes > 0) str += minutes + "m ";
 
                 elem.innerHTML = str + seconds + "s";
 
-                if (timeLeft <= 0) {
+                if (timeLeft <= 0)
+                {
                     clearInterval(timing);
                     elem.innerHTML = "It's over";
                 }
             }, 1000);
     }
-);
 
+})();
--- cncnet-api/resources/views/components/countdown.blade.php
+++ cncnet-api/resources/views/components/countdown.blade.php
@@ -4,16 +4,12 @@
 @endsection
 
 @section('footer')
-    <footer class="footer navbar-fixed-bottom collapse countdown">
-        <div class="container countdown">
-            <div class="row countdown">
-                <div class="col-md-8 text-right">
-                    <h1>Ladder Resets In: <span class="countdown-fill" id="laddercountdown" data-countdown-target="{{ $target }}"></span></h1>
-                </div>
-                <div class="col-md-4 text-left">
-                    <a class="btn btn-transparent close-countdown" href="#"><h4><i class="fa fa-times fa-lg" ></i></h4></a>
-                </div>
-            </div>
+    <footer class="footer navbar-fixed-bottom collapse countdown countdown-hidden">
+        <div>
+            <h1>Ladder Resets In: <span class="countdown-fill" id="laddercountdown" data-countdown-target="{{ $target }}"></span></h1>
+        </div>
+        <div>
+            <button class="btn btn-transparent close-countdown"><i class="fa fa-times fa-lg" ></i></button>
         </div>
     </footer>
 @endsection
