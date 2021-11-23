3055dd097c34cdb766e80fea9e657b5619aadaa2
Daniel Keeton Jr <dakeeton@gmail.com>
2021-11-22 21:40:58 -0600

    Delete the orphaned qm_match_states and map_side_strings in the cleanup commands

--- cncnet-api/app/Console/Commands/CleanupQmMatches.php
+++ cncnet-api/app/Console/Commands/CleanupQmMatches.php
@@ -8,43 +8,51 @@ use Carbon\Carbon;
 class CleanupQmMatches extends Command
 {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'QmMatches:prune';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'QmMatches:prune';
 
 	/**
-	 * Prune old QmMatches.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Command description.';
+     * Prune old QmMatches.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
 	{
-		parent::__construct();
-	}
+        parent::__construct();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
-		//Get QM Match Players from previous month
-		$date = Carbon::now()->startOfMonth();
-		$quickMatches = \App\QmMatch::where('created_at', '<', $date);
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //Get QM Match Players from previous month
+        $date = Carbon::now()->startOfMonth();
+        $quickMatches = \App\QmMatch::where('created_at', '<', $date);
+        echo "Deleting " . $quickMatches->count() . " records from qm_matches created before date $date\n";
+        $quickMatches->delete();
 
-		echo "Deleting " . $quickMatches->count() . " records from qm_matches created before date " . $date;
 
-		$quickMatches->delete();
-	}
+        // Delete orphaned qm_match_states
+        $quickMatchStates = \App\QmMatchState::leftJoin('qm_matches', function($join) {
+            $join->on('qm_matches.id', '=', 'qm_match_states.qm_match_id');
+        })->whereNull('qm_matches.id');
+
+        echo "Deleting " . $quickMatchStates->count() . " qm_match_states where qm_match is null\n";
+
+        $quickMatchStates->delete();
+    }
 }
