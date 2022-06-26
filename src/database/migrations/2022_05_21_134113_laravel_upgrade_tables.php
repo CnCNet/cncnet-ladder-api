05f9fae747be75aaaa84ead30508e017d1a14d35
Grant Bartlett <hello@grant-bartlett.com>
2022-06-26 17:47:20 +0100

    Removes migration, its handled by laravel auth

--- src/database/migrations/2022_05_21_134113_laravel_upgrade_tables.php
+++ src/database/migrations/2022_05_21_134113_laravel_upgrade_tables.php
@@ -20,17 +20,6 @@ class LaravelUpgradeTables extends Migration
             $table->timestamp('updated_at')->nullable()->default(null)->change();
             $table->timestamp('email_verified_at')->nullable()->default(null);
         });
-
-        Schema::create('personal_access_tokens', function (Blueprint $table)
-        {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamps();
-        });
     }
 
     /**
