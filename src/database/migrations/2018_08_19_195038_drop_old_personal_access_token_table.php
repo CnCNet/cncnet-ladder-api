4736fb03c93314866fc40dcea7622690abbf6ef6
Grant Bartlett <hello@grant-bartlett.com>
2022-08-19 20:53:44 +0100

    Drop old tokens table

---
+++ src/database/migrations/2018_08_19_195038_drop_old_personal_access_token_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        # Will be replaced by laravel 2019_12_14_000001_create_personal_access_tokens_table.php 
+        Schema::dropIfExists('personal_access_tokens');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
