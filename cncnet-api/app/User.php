8f78c81d5a5126ff80c2207a1f289a5943d851ba
alexp8 <amp1993@gmail.com>
2022-09-19 22:12:39 -0400

    merge

--- cncnet-api/app/User.php
+++ cncnet-api/app/User.php
@@ -240,6 +240,27 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         return [];
     }
 
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     public function userSettings() {
         return $this->hasOne('App\UserSettings');
     }
