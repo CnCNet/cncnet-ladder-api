694ed8e6911a4258a856b9c3c0a709fe93c9b36b
Grant Bartlett <hello@grant-bartlett.com>
2022-11-24 17:17:36 +0000

    Fix game url

--- cncnet-api/resources/views/ladders/player/_games.blade.php
+++ cncnet-api/resources/views/ladders/player/_games.blade.php
@@ -1,14 +1,14 @@
 <div class="table-responsive">
     <table class="games-table table align-middle mb-0">
         <tbody>
-            @foreach ($games as $game)
-                @php $pgr = \App\PlayerGameReport::where('game_report_id', $game->game_report_id)->get(); @endphp
-                @php $gr = \App\GameReport::where('id', $game->game_report_id)->first(); @endphp
-                @php $gameUrl = \App\URLHelper::getGameUrl($history, $game->id); @endphp
+            @foreach ($games as $gameReport)
+                @php $pgr = \App\PlayerGameReport::where('game_report_id', $gameReport->game_report_id)->get(); @endphp
+                @php $gr = \App\GameReport::where('id', $gameReport->game_report_id)->first(); @endphp
+                @php $gameUrl = \App\URLHelper::getGameUrl($history, $gameReport->game_id); @endphp
 
-                @foreach ($pgr as $k => $gamePlayer)
-                    @php $player = $gamePlayer->player()->first(); @endphp
-                    @php $playerUrl = \App\URLHelper::getPlayerProfileUrl($history, $gamePlayer->player->username); @endphp
+                @foreach ($pgr as $k => $gameReportPlayer)
+                    @php $player = $gameReportPlayer->player()->first(); @endphp
+                    @php $playerUrl = \App\URLHelper::getPlayerProfileUrl($history, $gameReportPlayer->player->username); @endphp
 
                     @if ($k == 0)
                         <tr class="align-middle">
@@ -18,30 +18,30 @@
                         <td class="td-player">
                             <div class="d-flex align-items-center">
                                 <div class="me-3">
-                                    <a href="{{ $playerUrl }}" title="View {{ $gamePlayer->player->username }}'s profile">
+                                    <a href="{{ $playerUrl }}" title="View {{ $gameReportPlayer->player->username }}'s profile">
                                         @include('components.avatar', ['avatar' => $player->user->getUserAvatar(), 'size' => 45])
                                     </a>
                                 </div>
 
                                 <div class="player-row">
                                     <div class="player-username">
-                                        <a href="{{ $playerUrl }}" title="View {{ $gamePlayer->player->username }}'s profile">
-                                            <p class="fw-bold mb-1">{{ $gamePlayer->player->username }}</p>
+                                        <a href="{{ $playerUrl }}" title="View {{ $gameReportPlayer->player->username }}'s profile">
+                                            <p class="fw-bold mb-1">{{ $gameReportPlayer->player->username }}</p>
                                         </a>
                                     </div>
 
-                                    @if ($gamePlayer->stats)
-                                        @php $playerStats2 = \App\Stats2::where("id", $gamePlayer->stats->id)->first(); @endphp
-                                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $gamePlayer->stats->cty); @endphp
+                                    @if ($gameReportPlayer->stats)
+                                        @php $playerStats2 = \App\Stats2::where("id", $gameReportPlayer->stats->id)->first(); @endphp
+                                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $gameReportPlayer->stats->cty); @endphp
                                         <div class="player-faction player-faction-{{ $playerCountry }}"></div>
 
-                                        <div class="game-status status-{{ $gamePlayer->won ? 'won' : 'lost' }}">
+                                        <div class="game-status status-{{ $gameReportPlayer->won ? 'won' : 'lost' }}">
                                             <span class="status-text">
-                                                {{ $gamePlayer->won == true ? 'Won' : 'Lost' }}
+                                                {{ $gameReportPlayer->won == true ? 'Won' : 'Lost' }}
                                             </span>
 
                                             <span class="points">
-                                                {{ $gamePlayer->points >= 0 ? "+$gamePlayer->points" : $gamePlayer->points }}
+                                                {{ $gameReportPlayer->points >= 0 ? "+$gameReportPlayer->points" : $gameReportPlayer->points }}
                                             </span>
                                         </div>
                                     @endif
@@ -52,28 +52,28 @@
                         <td class="td-player">
                             <div class="d-flex align-items-center">
                                 <div class="player-row">
-                                    @if ($gamePlayer->stats)
-                                        <div class="game-status status-{{ $gamePlayer->won ? 'won' : 'lost' }}">
+                                    @if ($gameReportPlayer->stats)
+                                        <div class="game-status status-{{ $gameReportPlayer->won ? 'won' : 'lost' }}">
                                             <span class="status-text">
-                                                {{ $gamePlayer->won == true ? 'Won' : 'Lost' }}
+                                                {{ $gameReportPlayer->won == true ? 'Won' : 'Lost' }}
                                             </span>
 
                                             <span class="points">
-                                                {{ $gamePlayer->points >= 0 ? "+$gamePlayer->points" : $gamePlayer->points }}
+                                                {{ $gameReportPlayer->points >= 0 ? "+$gameReportPlayer->points" : $gameReportPlayer->points }}
                                             </span>
                                         </div>
-                                        @php $playerStats2 = \App\Stats2::where("id", $gamePlayer->stats->id)->first(); @endphp
-                                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $gamePlayer->stats->cty); @endphp
+                                        @php $playerStats2 = \App\Stats2::where("id", $gameReportPlayer->stats->id)->first(); @endphp
+                                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $gameReportPlayer->stats->cty); @endphp
                                         <div class="player-faction player-faction-{{ $playerCountry }}"></div>
                                     @endif
                                     <div class="ms-3 me-3">
-                                        <a href="{{ $playerUrl }}" title="View {{ $gamePlayer->player->username }}'s profile">
+                                        <a href="{{ $playerUrl }}" title="View {{ $gameReportPlayer->player->username }}'s profile">
                                             @include('components.avatar', ['avatar' => $player->user->getUserAvatar(), 'size' => 45])
                                         </a>
                                     </div>
                                     <div class="player-username">
-                                        <a href="{{ $playerUrl }}" title="View {{ $gamePlayer->player->username }}'s profile">
-                                            <p class="fw-bold mb-1">{{ $gamePlayer->player->username }}</p>
+                                        <a href="{{ $playerUrl }}" title="View {{ $gameReportPlayer->player->username }}'s profile">
+                                            <p class="fw-bold mb-1">{{ $gameReportPlayer->player->username }}</p>
                                         </a>
                                     </div>
                                 </div>
@@ -91,8 +91,8 @@
                         <td>
                             <div class="d-flex align-items-center">
                                 <div>
-                                    <p class="fw-bold mb-1">{{ $game->scen }}</p>
-                                    <p class="text-muted mb-0"> {{ gmdate('H:i:s', $game->duration) }}</p>
+                                    <p class="fw-bold mb-1">{{ $gameReport->scen }}</p>
+                                    <p class="text-muted mb-0"> {{ gmdate('H:i:s', $gameReport->duration) }}</p>
                                 </div>
                             </div>
                         </td>
