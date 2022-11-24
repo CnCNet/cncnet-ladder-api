a073389b1fa984005ba734ffa1c7c348d7435456
Grant Bartlett <hello@grant-bartlett.com>
2022-11-24 16:44:23 +0000

    Add timestamp for sneer

--- cncnet-api/resources/views/components/game-box.blade.php
+++ cncnet-api/resources/views/components/game-box.blade.php
@@ -3,7 +3,7 @@
         <a href="{{ $url or '' }}" class="status status-{{ $status }}"></a>
     </div>
 
-    <a href="{{ $url or '' }}" class="game-box-link" data-toggle="tooltip" data-placement="top" title="View game">
+    <a href="{{ $url or '' }}" class="game-box-link" data-toggle="tooltip" data-placement="top" data-timestamp="{{ $date->timestamp }}" title="View game">
         <div class="details text-center">
             <h4 class="title">{{ $title }}</h4>
             <small class="status text-capitalize">{{ $status . ' ' . $date->diffForHumans() }}</small>
@@ -23,26 +23,27 @@
 
         <div class="footer text-center {{ $history->ladder->abbreviation }}">
             <?php $gamePlayerResults = $gamePlayers->get(); ?>
-            @foreach($gamePlayerResults as $k => $gamePlayer)
+            @foreach ($gamePlayerResults as $k => $gamePlayer)
+                <div class="player {{ $gamePlayer->won == true ? 'won' : 'lost' }} player-order-{{ $k }}">
 
-                <div class="player {{ $gamePlayer->won == true ? 'won': 'lost' }} player-order-{{ $k }}">
-
-                    @if($gamePlayer->stats)
-                    @php $playerStats2 = \App\Stats2::where("id", $gamePlayer->stats->id)->first(); @endphp
-                    @php $playerCountry = $playerStats2->faction($history->ladder->game, $gamePlayer->stats->cty); @endphp
-                    <div class="player-faction player-faction-{{ $playerCountry }}"></div>
+                    @if ($gamePlayer->stats)
+                        @php $playerStats2 = \App\Stats2::where("id", $gamePlayer->stats->id)->first(); @endphp
+                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $gamePlayer->stats->cty); @endphp
+                        <div class="player-faction player-faction-{{ $playerCountry }}"></div>
                     @endif
-                    
+
                     <h5>
-                        {{ $gamePlayer->player->username }} 
+                        {{ $gamePlayer->player->username }}
                         <span class="points">
-                        @if ($gamePlayer->points >= 0)+@endif{{ $gamePlayer->points }}
+                            @if ($gamePlayer->points >= 0)
+                                +
+                            @endif{{ $gamePlayer->points }}
                         </span>
                     </h5>
                 </div>
 
-                @if($k == 0)
-                <p class="vs">vs</p>
+                @if ($k == 0)
+                    <p class="vs">vs</p>
                 @endif
             @endforeach
         </div>
