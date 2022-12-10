504cfd4a92ed3d5d31ffb6102054c82ace8452d4
Grant Bartlett <hello@grant-bartlett.com>
2022-12-10 11:47:59 +0000

    Bug fixes and show admin tools

--- cncnet-api/resources/views/ladders/game-view.blade.php
+++ cncnet-api/resources/views/ladders/game-view.blade.php
@@ -80,7 +80,7 @@
 @endsection
 
 @section('content')
-    @if (\Auth::user() && \Auth::user()->isAdmin())
+    @if (\Auth::user() && \Auth::user()->isModerator())
         <div class="container mt-5 mb-5">
             <a class="btn btn-outline" data-bs-toggle="collapse" data-bs-target="#adminTools" aria-expanded="false" aria-controls="adminTools">
                 Show admin tools
--- cncnet-api/resources/views/ladders/game/_map-with-players.blade.php
+++ cncnet-api/resources/views/ladders/game/_map-with-players.blade.php
@@ -1,29 +1,43 @@
 @php
-    $mapPreview = 'https://ladder.cncnet.org/images/maps/' . $history->ladder->abbreviation . '/' . $map->hash . '.png';
+    $mapPreview = '';
     
-    $mapPreviewSize = getimagesize($mapPreview);
-    $webMapWidth = $mapPreviewSize[0];
-    $webMapHeight = $mapPreviewSize[1];
+    try {
+        $mapPreview = 'https://ladder.cncnet.org/images/maps/' . $history->ladder->abbreviation . '/' . $map->hash . '.png';
+        $mapPreviewSize = getimagesize($mapPreview);
+        $webMapWidth = $mapPreviewSize[0];
+        $webMapHeight = $mapPreviewSize[1];
     
-    $mapStartX = $map->mapHeaders->startX ?? -1;
-    $mapStartY = $map->mapHeaders->startY ?? -1;
-    $mapWidth = $map->mapHeaders->width ?? -1;
-    $mapHeight = $map->mapHeaders->height ?? -1;
-    $ratioX = $webMapWidth / $mapWidth;
-    $ratioY = $webMapHeight / $mapHeight;
+        $mapStartX = $map->mapHeaders->startX ?? -1;
+        $mapStartY = $map->mapHeaders->startY ?? -1;
+        $mapWidth = $map->mapHeaders->width ?? -1;
+        $mapHeight = $map->mapHeaders->height ?? -1;
+        $ratioX = $webMapWidth / $mapWidth;
+        $ratioY = $webMapHeight / $mapHeight;
+    
+        $hasMapData = true;
+    } catch (\Exception $ex) {
+        $hasMapData = false;
+    }
 @endphp
 
 <div class="container">
     <div class="d-flex justify-content-center">
-        @include('ladders.game._map-preview', [
-            'playerGameReports' => $playerGameReports,
-            'mapStartX' => $mapStartX,
-            'mapStartY' => $mapStartY,
-            'mapPreview' => $mapPreview,
-            'mapWidth' => $mapWidth,
-            'mapHeight' => $mapHeight,
-            'ratioX' => $ratioX,
-            'ratioY' => $ratioY,
-        ])
+
+        @if ($hasMapData)
+            @include('ladders.game._map-preview', [
+                'playerGameReports' => $playerGameReports,
+                'mapStartX' => $mapStartX,
+                'mapStartY' => $mapStartY,
+                'mapPreview' => $mapPreview,
+                'mapWidth' => $mapWidth,
+                'mapHeight' => $mapHeight,
+                'ratioX' => $ratioX,
+                'ratioY' => $ratioY,
+            ])
+        @else
+            <div class="map-preview d-lg-none">
+                <img src="{{ $mapPreview }}" style="max-width:100%" />
+            </div>
+        @endif
     </div>
 </div>
