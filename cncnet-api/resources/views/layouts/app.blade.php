93870cf003cb1fafffc099039546b226062a08a2
Grant Bartlett <hello@grant-bartlett.com>
2022-12-23 18:52:03 +0000

    Bump css

--- cncnet-api/resources/views/layouts/app.blade.php
+++ cncnet-api/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <meta property="og:url" content="{{ Request::url() }}" />
     <meta property="og:type" content="website" />
 
-    <link rel="stylesheet" href="/css/app.css?v=2.0.6" />
+    <link rel="stylesheet" href="/css/app.css?v=2.0.7" />
     <link rel="apple-touch-icon" sizes="152x152" href="/images/meta/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/images/meta/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/images/meta/favicon-16x16.png">
@@ -64,9 +64,10 @@ body-feature-image
 
     @include('components.footer')
     @yield('footer')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous">
     </script>
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="/js/cncnet-online.js"></script>
 
     <script>
