998fdffb5e0e34846fa81b0eabebf11792e5e256
alexp8 <amp1993@gmail.com>
2023-06-19 11:39:36 -0400

    add stars to map name in qm client

--- cncnet-api/app/Http/Controllers/ApiQuickMatchController.php
+++ cncnet-api/app/Http/Controllers/ApiQuickMatchController.php
@@ -212,7 +212,6 @@ class ApiQuickMatchController extends Controller
 
     public function mapListRequest(Request $request, $ladderAbbrev = null)
     {
-        //$qmMaps = \App\QmMap::where('ladder_id', $this->ladderService->getLadderByGame($ladderAbbrev)->id)->get();
         return \App\QmMap::findMapsByLadder($this->ladderService->getLadderByGame($ladderAbbrev)->id);
     }
 
