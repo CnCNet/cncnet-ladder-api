18c7b12c8cd7dddf61acd357807332465dc97ad6
Grant Bartlett <hello@grant-bartlett.com>
2024-04-30 19:22:18 +0100

    Reduce size of player-card

--- cncnet-api/resources/views/ladders/game/_player-card.blade.php
+++ cncnet-api/resources/views/ladders/game/_player-card.blade.php
@@ -1,26 +1,27 @@
 <div class="player-card">
     <div class="player-avatar" style="width:120px;height:120px;">
-        <a href="{{ \App\Models\URLHelper::getPlayerProfileUrl($history, $player->username) }}"
-           title="View {{ $player->username }}'s profile">
+        <a href="{{ \App\Models\URLHelper::getPlayerProfileUrl($history, $player->username) }}" title="View {{ $player->username }}'s profile">
 
-            @include('components.avatar', ['avatar' => $player->user->getUserAvatar(), 'size' => 120])
+            <div class="avatar-with-faction">
+                @include('components.avatar', ['avatar' => $player->user->getUserAvatar(), 'size' => 120])
+
+                <div class="faction mt-2" style="padding-left:0;">
+                    @if ($pgr->stats)
+                        @php $playerStats2 = \App\Models\Stats2::where("id", $pgr->stats->id)->first(); @endphp
+                        @php $playerCountry = $playerStats2->faction($history->ladder->game, $pgr->stats->cty); @endphp
+                        <div class="{{ $history->ladder->game }} player-faction player-faction-{{ $playerCountry }}"></div>
+                    @endif
+                </div>
+            </div>
         </a>
     </div>
 
-    <a href="{{ \App\Models\URLHelper::getPlayerProfileUrl($history, $player->username) }}"
-       title="View {{ $player->username }}'s profile">
-
-        <div class="h3 mt-2 username">
+    <a href="{{ \App\Models\URLHelper::getPlayerProfileUrl($history, $player->username) }}" title="View {{ $player->username }}'s profile">
+        <h4 class="mt-2 mb-0 username">
             {{ $player->username }}
-        </div>
+        </h4>
 
-        @if (isset($playerRank))
-            <h5 class="rank pb-1 pt-2">
-                Rank #{{ $playerRank }}
-            </h5>
-        @endif
-
-        <div class="points font-secondary-bold {{ $pgr->won ? 'won' : 'lost' }}">
+        <div class="pt-2 pb-2 points font-secondary-bold {{ $pgr->won ? 'won' : 'lost' }}">
             @if ($pgr->points > 0)
                 <strong class="me-1">Won {{ '+' }}</strong>
             @else
@@ -29,24 +30,12 @@
             {{ $pgr->points }} points
         </div>
 
-        <div class="faction mt-2" style="padding-left:0;">
-            @if ($pgr->stats)
-                @php $playerStats2 = \App\Models\Stats2::where("id", $pgr->stats->id)->first(); @endphp
-                @php $playerCountry = $playerStats2->faction($history->ladder->game, $pgr->stats->cty); @endphp
-                <div class="{{ $history->ladder->game }} player-faction player-faction-{{ $playerCountry }}"></div>
-            @endif
-        </div>
+        @if (isset($playerRank))
+            <h6 class="rank">
+                Rank #{{ $playerRank }}
+            </h6>
+        @endif
     </a>
-
-    {{-- 
-    <div class="player-details">
-        <h2 class="username">
-            <a href="{{ \App\URLHelper::getPlayerProfileUrl($history, $player->username) }}" title="View {{ $player->username }}'s profile">
-                {{ $player->username }}
-            </a>
-        </h2>
---}}
-
 </div>
 
 
@@ -54,7 +43,7 @@
     <div class="m-auto text-center mt-4">
         @if (!$history->ladder->clans_allowed)
             <div style="font-size: 1rem" class="mt-2 mb-2 font-secondary-bold">
-                    <?php $tier = $player->getCachedPlayerTierByLadderHistory($history); ?>
+                <?php $tier = $player->getCachedPlayerTierByLadderHistory($history); ?>
                 <span class="me-2">
                     {!! \App\Helpers\LeagueHelper::getLeagueIconByTier($tier) !!}
                 </span>
