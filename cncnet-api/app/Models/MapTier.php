a1a6543b68a4f1fc9595a0f8baae46deffc8defb
alexp8 <amp1993@gmail.com>
2024-05-27 18:07:29 -0400

    update maptier

--- cncnet-api/app/Models/MapTier.php
+++ cncnet-api/app/Models/MapTier.php
@@ -6,6 +6,14 @@ class MapTier extends Model
 {
     public $timestamps = false;
 
+    
+    protected $fillable = [
+        'name',
+        'map_pool_id',
+        'max_vetoes',
+        'tier'
+    ];
+
     public function mapPool()
     {
         return $this->belongsTo(MapPool::class);
