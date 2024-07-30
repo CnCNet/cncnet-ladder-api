a50df1dfe3ad12a599951663907307f2560cada7
alexp8 <amp1993@gmail.com>
2024-07-30 11:12:58 -0400

    toggle off log

--- cncnet-api/app/Models/Ban.php
+++ cncnet-api/app/Models/Ban.php
@@ -81,7 +81,7 @@ class Ban extends Model
 
     public function checkStartBan($startBanStraightAway = false)
     {
-        Log::debug("checkStartBan: ban_id=" . $this->id . ", startBanStraightAway=" . $startBanStraightAway . ", ban_type=" . $this->ban_type . ", desc=" . Ban::typeToDescription($this->ban_type) . ", expires=" . $this->expires);
+        // Log::debug("checkStartBan: ban_id=" . $this->id . ", startBanStraightAway=" . $startBanStraightAway . ", ban_type=" . $this->ban_type . ", desc=" . Ban::typeToDescription($this->ban_type) . ", expires=" . $this->expires);
 
         $banned = false;
         $cooldown = false;
