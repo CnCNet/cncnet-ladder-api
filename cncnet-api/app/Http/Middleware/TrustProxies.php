7b39c242ce87abc4175c81510b50bfbc88db8fb2
rohs <sylvain@rohs.ch>
2025-06-08 17:26:42 +0200

    cd: fix trusted proxies

--- cncnet-api/app/Http/Middleware/TrustProxies.php
+++ cncnet-api/app/Http/Middleware/TrustProxies.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 class TrustProxies extends Middleware
 {
     /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*';
+
+    /**
      * The headers that should be used to detect proxies.
      *
      * @var string
