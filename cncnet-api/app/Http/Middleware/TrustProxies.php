954d3ad64718bbbac794c7afc81216d6933adb47
rohs <sylvain@rohs.ch>
2025-06-08 17:25:23 +0200

    cd: fix trusted proxies

--- cncnet-api/app/Http/Middleware/TrustProxies.php
+++ cncnet-api/app/Http/Middleware/TrustProxies.php
@@ -7,16 +7,17 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-
     /**
      * The headers that should be used to detect proxies.
      *
      * @var string
      */
     protected $headers =
+        Request::HEADER_FORWARDED |
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_PREFIX |
         Request::HEADER_X_FORWARDED_AWS_ELB;
 }
