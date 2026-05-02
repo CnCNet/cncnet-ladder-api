9770d8de104f33f22f8108dcf3628a6dbf9ece67
alexp8 <amp1993@gmail.com>
2026-05-02 08:11:05 -0400

    laravel fix

--- cncnet-api/bootstrap/providers.php
+++ cncnet-api/bootstrap/providers.php
@@ -5,12 +5,7 @@ return [
     App\Providers\EventServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
 
-    Barryvdh\Debugbar\ServiceProvider::class,
-
     App\Providers\Navigation::class,
     App\Providers\IrcCache::class,
     App\LockedCache\LockedCacheServiceProvider::class,
-
-    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-    Intervention\Image\ImageServiceProvider::class,
 ];
